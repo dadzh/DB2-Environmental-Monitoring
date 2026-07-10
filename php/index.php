ad3b1ef3ab841933204e50159154ee8abfaf6b14
dadzh <32393599+dadzh@users.noreply.github.com>
2026-07-10 21:08:37 +0200

    Create index.php

---
+++ php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Main</title>
+</head>
+<body>
+    <h1>DataBase MOD B Project</h1>
+    <ul>
+        <li><a href="sql.php">MySQL - Air Quality Data</a></li>
+        <li><a href="mongo.php">MongoDB - Temperature and Humidity Data</a></li>
+        <li><a href="neo4j.php">Neo4j - Sensor Room Device Graph</a></li>
+    </ul>
+</body>
+</html>
